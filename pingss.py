9cf52aac9a5d7290f3e3e9f46e74faf6705592db
Eduardo Garcia <eduardo.garcia151@gmail.com>
2023-01-01 00:07:58 -0400

    UPDATED Ping Feature

--- pingss.py
+++ pingss.py
@@ -32,27 +32,67 @@ def bynet():
     os.system('clear')
     f = Figlet(font='standard')
     print(f.renderText('Ping IP'))
-    ipnet = input("Please enter Network Gateway: ")
+    IP = input("[+] Enter the Host IP Address:\t")
+    ipnetpar = IP.split('.')
+    IPs=IP
+
+    dot = IP.rfind(".")
+    IP = IP[0:dot + 1]
+
     ipsub = input("Please enter subnet: ")
-    print('Pass')
+
+    live = 0
+    dead = 0
+
+    start = int(ipnetpar[3])
+    end = 1
+    hosts='a'
+    
+    if ipsub == '28':
+        end = start + 14
+        if end < 255:
+         hosts = str(ipnetpar[0])+'.'+str(ipnetpar[1])+'.'+str(ipnetpar[2])+'.'+str(end)
+    print("Starting Ping Sweeper on " + IPs + " To " + hosts)
+
+    for i in range(start, end):
+        host = IP + str(i)
+        try:
+           response = subprocess.check_output(['ping', '-c 1', '-W 1' , host]) 
+           live = live + 1
+           print(host + ' is live')
+        except:
+            dead = dead + 1
+            print(host)
+    print(str(live) + ' host are up and ' + str(dead) +' host are down')
+    input("Press Any Key to Continue...")
+    pingmain(0)
+
+
+
+
+
+
+
 
 def selected():
     os.system('clear')
     f = Figlet(font='standard')
     print(f.renderText('Ping IP'))
     ipnet = input("Please enter IP: ")
-    output = subprocess.check_output(['ping', '-c 4', ipnet]) 
-    print(output) 
     try:
-        output = subprocess.check_output(['ping', '-c 4', ipnet]) 
-        if '0 received' in output: 
-            print('IP unreachable') 
-        else: 
-            print('IP reachable') 
-        print(output) 
-    except: 
-        print('IP unreachabless') 
-    print('Pass')
-    pingmain(1)
+        outputs = subprocess.check_output(['ping', '-c 1', ipnet])
+        subprocess.run(['ping', '-c 4', ipnet])
+        if b'0 packets received' in outputs:
+            print("IP Not Live")
+        else:
+            print()
+            print()
+            print('IP is Live')
+    except:
+        print()
+        print('Not Found')
+    print()
+    input("Press any key to Continue...")
+    pingmain(0)
 
 
