1e51c25a855d67f4f67c12febdd32dab70e62287
Eduardo Garcia <eduardo.garcia151@gmail.com>
2023-01-01 13:38:33 -0400

    Added Port Scanner Module

--- portscan.py
+++ portscan.py
@@ -17,5 +17,22 @@ def portmain():
     print("_" * 60)
     print ("Please wait, scanning remote host", remoteServerIP)
     print ("_" *60)
+    t1 = datetime.now()
+    try:
+        for port in range (1,5000):
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((remoteServerIP, port))
+            if result ==0:
+                print ("Port {}:        Open".format(port))
+                sock.close()  
+    except KeyboardInterrupt:
+        print ("You pressed Ctrl+C")
+        sys.exit()
+    except socket.gaierror:
+        print ("Hostname could not be resolved. Exiting")
+        sys.exit()
+    except socket.error:
+        print ("Couldn't connect to server")
+        sys.exit()
     input("Press any key to continue...")
 
